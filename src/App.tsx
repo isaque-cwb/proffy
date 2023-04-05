3a473066c492aeadae88f153a7564bc6c7df916e
isaque-cwb <isaque.lourenco2@gmail.com>
2023-04-05 19:41:06 -0300

    Projeto Criado

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './assets/styles/global.css';
+import Landing from './pages/Landing'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
