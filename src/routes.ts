a102b9f206462c738a8fbe6040acef6916996fe9
isaque-cwb <isaque.lourenco2@gmail.com>
2023-05-08 11:53:38 -0300

    API Finalizado

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,21 @@
 import express from 'express'
+import ClassesController from './controllers/ClassesControllers';
+import ConnectionController from './controllers/ConnectionsController';
+
+
 
 const routes = express.Router();
+const classesControllers = new ClassesController()
+const connectionsController = new ConnectionController()
 
 
 
-routes.get('/users', (req, res)=>{
+routes.post('/classes', classesControllers.create)
+routes.get('/classes', classesControllers.index)
 
-    return res.json({ message: 'Hello World!!!'})
-})
 
+routes.post('/connections', connectionsController.create)
+routes.get('/connections', connectionsController.index)
 
 export default routes;
 
