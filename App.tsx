c05b02cfcdc43b61e7892843328b5f6cff61893a
isaque-cwb <isaque.lourenco2@gmail.com>
2023-05-16 13:04:20 -0300

    alterando ordem commit

--- App.tsx
+++ App.tsx
@@ -7,19 +7,48 @@ import { useCallback, useEffect, useState } from 'react';
 import * as Font from 'expo-font';
 import { View } from 'react-native';
 
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
 
- 
-  
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await Font.loadAsync({
+          Archivo_400Regular,
+          Archivo_700Bold,
+          Poppins_400Regular,
+          Poppins_600SemiBold
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
 
   return (
-    < >
+    <View  onLayout={onLayoutRootView} style={{flex:1}}>
       <Landing />
       <StatusBar style="light" />
-    </>
+    </View>
   );
 
+  
 
-}
-
+  }
--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -10,7 +10,25 @@ function Landing(){
     return(
        
        <View style={styles.container} >
-        <Text>Hello World!</Text>
+        <Image source={landingImage} style={styles.banner}/>
+        <Text style={styles.title} >
+            Seja bem-vindo, {'\n'}
+            <Text style={styles.titleBold} >O que deseja fazer?</Text>
+        </Text>
+        <View style={styles.buttonContainer}>
+            <TouchableOpacity style={[styles.button, styles.buttonPrimary]} >
+            <Image source={studyIcon}/>
+            <Text style={styles.buttonText}>Estudar</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={[styles.button, styles.buttonSecondary]} >
+            <Image source={giveClassesIcon}/>
+            <Text style={styles.buttonText}>Dar Aulas</Text>
+            </TouchableOpacity>
+        </View>
+        <Text  style={styles.totalConnections}>
+            Total de 285 conexões já realizadas.
+            <Image source={heartIcon} />
+        </Text>
        </View>
 
     )
