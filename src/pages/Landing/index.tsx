d507ccff6e4190ff608d8b129cdd58fbe69f6ab4
isaque-cwb <isaque.lourenco2@gmail.com>
2023-05-16 12:53:21 -0300

    Projeto Criado

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -10,25 +10,7 @@ function Landing(){
     return(
        
        <View style={styles.container} >
-        <Image source={landingImage} style={styles.banner}/>
-        <Text style={styles.title} >
-            Seja bem-vindo, {'\n'}
-            <Text style={styles.titleBold} >O que deseja fazer?</Text>
-        </Text>
-        <View style={styles.buttonContainer}>
-            <TouchableOpacity style={[styles.button, styles.buttonPrimary]} >
-            <Image source={studyIcon}/>
-            <Text style={styles.buttonText}>Estudar</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={[styles.button, styles.buttonSecondary]} >
-            <Image source={giveClassesIcon}/>
-            <Text style={styles.buttonText}>Dar Aulas</Text>
-            </TouchableOpacity>
-        </View>
-        <Text  style={styles.totalConnections}>
-            Total de 285 conexões já realizadas.
-            <Image source={heartIcon} />
-        </Text>
+        <Text>Hello World!</Text>
        </View>
 
     )
