a9949065ec8a5e49e4745247421e05289dc79886
unknown <Administrador@CWB-15017.tequaly.local>
2023-06-03 21:02:51 -0300

    conectando com API e total de conexões

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, Text, Image, TouchableOpacity } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import styles from './styles'
@@ -7,6 +7,7 @@ import studyIcon from '../../assets/images/icons/study.png'
 import giveClassesIcon from '../../assets/images/icons/give-classes.png'
 import heartIcon from '../../assets/images/icons/heart.png'
 import { RectButton } from 'react-native-gesture-handler'
+import api from './../../services/api';
 
 
 
@@ -14,6 +15,17 @@ function Landing(): JSX.Element{
 
 
     const {navigate} = useNavigation()
+
+    const [ totalConnections, setTotalConnections ] = useState(0)
+    
+    useEffect(()=>{
+        api.get('connections').then(response=>{
+            const { total } = response.data
+            setTotalConnections(total)
+        })
+    },[])
+
+
 
     function handleNavigateToGiveClassesPage(){
         navigate('GiveClasses')
@@ -47,7 +59,7 @@ function Landing(): JSX.Element{
             </RectButton>
         </View>
         <Text  style={styles.totalConnections}>
-            Total de 285 conexões já realizadas.
+            Total de {totalConnections} conexões já realizadas.
             <Image source={heartIcon} />
         </Text>
        </View>
